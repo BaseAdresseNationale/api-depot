c505882039d015f7cf190a9bbfa52496d67ea356
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-06-22 18:33:52 +0200

    Add route to validate BALC

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const createError = require('http-errors')
 const errorHandler = require('./lib/util/error-handler')
+const {isCommuneActuelle} = require('./lib/util/cog')
+const rawBodyParser = require('./lib/util/raw-body-parser')
+const {validateBAL} = require('./lib/validate-bal')
 
 const app = express()
 
@@ -13,6 +17,19 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(cors({origin: true}))
+
+app.use('/commune/:codeCommune', (req, res, next) => {
+  if (!isCommuneActuelle(req.params.codeCommune)) {
+    return next(createError(404, 'Le code commune n’existe pas ou n’est plus en vigueur'))
+  }
+
+  req.codeCommune = req.params.codeCommune
+  next()
+})
+
+app.post('/commune/:codeCommune/validate', rawBodyParser(), validateBAL(), (req, res) => {
+  res.send({validation: req.validationResult})
+})
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
