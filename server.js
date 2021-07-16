9575e5975ca8b613936301579744f35d6c327c9b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-07-16 18:27:55 +0200

    Add revision creation route

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const {isString, isPlainObject, create} = require('lodash')
 const createError = require('http-errors')
 const errorHandler = require('./lib/util/error-handler')
 const {isCommuneActuelle} = require('./lib/util/cog')
+const w = require('./lib/util/w')
 const rawBodyParser = require('./lib/util/raw-body-parser')
 const {validateBAL} = require('./lib/validate-bal')
+const {authClient} = require('./lib/clients')
+const {createRevision} = require('./lib/revisions')
 
 const app = express()
 
@@ -27,6 +31,31 @@ app.param('codeCommune', (req, res, next) => {
   req.codeCommune = req.params.codeCommune
   next()
 })
+
+app.post('/communes/:codeCommune/revisions', authClient(), w(async (req, res) => {
+  // {nomComplet, organisation, extras, codeCommune, client}
+  if ('nomComplet' in req.body && !isString(req.body.nomComplet)) {
+    throw createError(400, 'Le contenu de la requête est invalide (nomComplet)')
+  }
+
+  if ('organisation' in req.body && !isString(req.body.organisation)) {
+    throw createError(400, 'Le contenu de la requête est invalide (organisation)')
+  }
+
+  if ('extras' in req.body && !isPlainObject(req.body.extras)) {
+    throw createError(400, 'Le contenu de la requête est invalide (extras)')
+  }
+
+  const revision = await createRevision({
+    nomComplet: req.body.nomComplet,
+    organisation: req.body.organisation,
+    extras: req.body.extras,
+    codeCommune: req.codeCommune,
+    client: req.client
+  })
+
+  res.status(201).send(revision)
+}))
 
 app.post('/commune/:codeCommune/validate', rawBodyParser(), validateBAL(), (req, res) => {
   res.send({validation: req.validationResult})
