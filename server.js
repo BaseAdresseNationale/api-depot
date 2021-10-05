5ee20f6ed899a966717b386eef681d5de94c47df
tmerlier <theophile.merliere@gmail.com>
2021-10-05 11:15:21 +0200

    Merge start.js and server.js

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 #!/usr/bin/env node
 require('dotenv').config()
 
+const mongo = require('./lib/util/mongo')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const {revisionsRoutes} = require('./lib/revisions/routes')
 
-async function createServer(params) {
+async function main() {
   const app = express()
+  const port = process.env.PORT || 5000
 
   app.use(express.json())
   app.use(cors({origin: true}))
+
+  await mongo.connect()
 
   if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'))
   }
 
-  const revisions = await revisionsRoutes(params)
+  const revisions = await revisionsRoutes()
   app.use('/', revisions)
 
-  return app
+  app.listen(port, () => {
+    console.log(`Start listening on port ${port}`)
+  })
 }
 
-module.exports = {createServer}
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
