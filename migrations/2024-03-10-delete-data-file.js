9304fa363f6a63df95022ba9db67c1d47cfebe8a
Fufeck <fabien.tafforeau@gmail.com>
2024-04-03 13:13:09 +0200

    lint: add eslint and prettier

--- migrations/2024-03-10-delete-data-file.js
+++ migrations/2024-03-10-delete-data-file.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
-require('dotenv').config()
-const mongo = require('../lib/util/mongo')
+require("dotenv").config();
+const mongo = require("../lib/util/mongo");
 
 async function main() {
-  await mongo.connect()
+  await mongo.connect();
 
-  await mongo.db.collection('files').updateMany({}, {$unset: {data: 1}})
+  await mongo.db.collection("files").updateMany({}, { $unset: { data: 1 } });
 
-  await mongo.disconnect()
+  await mongo.disconnect();
 }
 
-main().catch(error => {
-  console.error(error)
-  process.exit(1)
-})
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
--- scripts/revalidate-revisions.js
+++ scripts/revalidate-revisions.js
@@ -1,49 +1,64 @@
 #!/usr/bin/env node
 /* eslint no-await-in-loop: off */
-require('dotenv').config()
+require("dotenv").config();
 
-const validatorVersion = require('@ban-team/validateur-bal/package.json').version
-const mongo = require('../lib/util/mongo')
-const {fetchRevision, getFiles, getFileData} = require('../lib/revisions/model')
-const {applyValidateBAL} = require('../lib/revisions/validate-bal')
+const validatorVersion =
+  require("@ban-team/validateur-bal/package.json").version;
+const mongo = require("../lib/util/mongo");
+const {
+  fetchRevision,
+  getFiles,
+  getFileData,
+} = require("../lib/revisions/model");
+const { applyValidateBAL } = require("../lib/revisions/validate-bal");
 
 async function main() {
-  await mongo.connect()
+  await mongo.connect();
 
   const query = {
-    status: 'published',
+    status: "published",
     current: true,
-    'validation.validatorVersion': {$ne: validatorVersion}
-  }
+    "validation.validatorVersion": { $ne: validatorVersion },
+  };
 
-  const revisionsToRevalidate = await mongo.db.collection('revisions')
-    .distinct('_id', query)
+  const revisionsToRevalidate = await mongo.db
+    .collection("revisions")
+    .distinct("_id", query);
 
   for (const revisionId of revisionsToRevalidate) {
-    console.log(revisionId)
-    const revision = await fetchRevision(revisionId)
-    const files = await getFiles(revision)
+    console.log(revisionId);
+    const revision = await fetchRevision(revisionId);
+    const files = await getFiles(revision);
 
-    const balFile = files.find(f => f.type === 'bal')
-    const data = await getFileData(balFile._id)
-    const relaxMode = revision.client.options?.relaxMode
-    const {validation} = await applyValidateBAL(data, revision.codeCommune, {relaxMode})
-    const {errors, warnings, infos, rowsCount} = validation
+    const balFile = files.find((f) => f.type === "bal");
+    const data = await getFileData(balFile._id);
+    const relaxMode = revision.client.options?.relaxMode;
+    const { validation } = await applyValidateBAL(data, revision.codeCommune, {
+      relaxMode,
+    });
+    const { errors, warnings, infos, rowsCount } = validation;
 
-    await mongo.db.collection('revisions').updateOne(
-      {_id: revisionId},
-      {$set: {
-        validation: {
-          valid: true, validatorVersion, errors, warnings, infos, rowsCount
-        }
-      }}
-    )
+    await mongo.db.collection("revisions").updateOne(
+      { _id: revisionId },
+      {
+        $set: {
+          validation: {
+            valid: true,
+            validatorVersion,
+            errors,
+            warnings,
+            infos,
+            rowsCount,
+          },
+        },
+      },
+    );
   }
 
-  await mongo.disconnect()
+  await mongo.disconnect();
 }
 
-main().catch(error => {
-  console.error(error)
-  process.exit(1)
-})
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
