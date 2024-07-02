d7f9861eabf1132175004ec348f9a953fafb3ca1
Fufeck <fabien.tafforeau@gmail.com>
2024-07-02 17:18:17 +0200

    hotfix: add userProfile in strategy

--- src/modules/habilitation/france_connect/france_connect.strategy.ts
+++ src/modules/habilitation/france_connect/france_connect.strategy.ts
@@ -1,14 +1,22 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Strategy } from 'passport-oauth2';
 import { ConfigService } from '@nestjs/config';
+import { callbackify } from 'util';
+import { AxiosError, AxiosRequestConfig } from 'axios';
+import { UserFranceConnect } from '@/lib/types/user_france_connect.type';
+import { catchError, firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class FranceConnectStrategy extends PassportStrategy(
   Strategy,
   'oauth2',
 ) {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private httpService: HttpService,
+  ) {
     super(
       {
         authorizationURL:
@@ -31,4 +39,29 @@ export class FranceConnectStrategy extends PassportStrategy(
   authorizationParams = function () {
     return { nonce: 'foobar' };
   };
+
+  userProfile = callbackify(async (token) => {
+    console.log('callbackify');
+    const url: string = `${this.configService.get<string>('FC_SERVICE_URL')}/api/v1/userinfo?schema=openid`;
+    const options: AxiosRequestConfig = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      responseType: 'json',
+    };
+
+    const { data } = await firstValueFrom(
+      this.httpService.get<UserFranceConnect>(url, options).pipe(
+        catchError((error: AxiosError) => {
+          console.error(error);
+          throw new HttpException(
+            'Impossible de récupérer le profile',
+            HttpStatus.FAILED_DEPENDENCY,
+          );
+        }),
+      ),
+    );
+    console.log(data);
+    return data;
+  });
 }
