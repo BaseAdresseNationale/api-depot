20db90c628984dc3646fbdab73548b866fe8aa45
Fufeck <fabien.tafforeau@gmail.com>
2026-08-10 16:08:24 +0200

    fix: #YWH-PGM44513-16

--- src/modules/habilitation/pro_connect/pro_connect.guard.ts
+++ src/modules/habilitation/pro_connect/pro_connect.guard.ts
@@ -5,19 +5,43 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ProConnectAuthGuard extends AuthGuard('pro_connect_oauth2') {
+  constructor(private configService: ConfigService) {
+    super();
+  }
+
+  private isRedirectUrlAllowed(redirectUrl: string): boolean {
+    const redirectUrlWhiteList: string[] =
+      this.configService.get('REDIRECT_URL_WHITE_LIST')?.split(',') || [];
+
+    try {
+      const { origin } = new URL(redirectUrl);
+      return redirectUrlWhiteList.some(
+        (whiteListedUrl: string) => new URL(whiteListedUrl).origin === origin,
+      );
+    } catch {
+      return false;
+    }
+  }
+
   canActivate(context: ExecutionContext) {
     const req = context.getArgByIndex(0);
     const res = context.getArgByIndex(1);
-
     if (!req.query.redirectUrl) {
       return res.status(400).send('redirectUrl param is mandatory');
     }
 
+    const decodedRedirectUrl = decodeURIComponent(req.query.redirectUrl);
+
+    if (!this.isRedirectUrlAllowed(decodedRedirectUrl)) {
+      return res.status(400).send('redirectUrl param is not correct');
+    }
+
     req.session.habilitationId = req.params.habilitationId;
-    req.session.redirectUrl = decodeURIComponent(req.query.redirectUrl);
+    req.session.redirectUrl = decodedRedirectUrl;
 
     if (req.habilitation.status !== 'pending') {
       return res.redirect(req.session.redirectUrl);
